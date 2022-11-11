7e182ce9c48f61ac80bf0818ade8261fb08637fd
Avinash <97391739+Avinash7390@users.noreply.github.com>
2022-11-12 02:16:57 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//import java.util.*;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        double num=180.0;
+//        double radiuns= Math.toRadians(num);
+//
+//
+//
+//        System.out.println(Math.tan(radiuns));
+//    }
+//}
